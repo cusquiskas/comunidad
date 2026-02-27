7ee0acd6cc50eacaa4416843c446e18097021e5c
José Miguel Munar <thrawn@MacBook-Air-de-Jose.local>
2026-02-27 20:28:48 +0100

    ya repartimos la derrama perfectamente, tanto a partes iguales como por coeficiente

--- content/back/gastos/derramaAPago.js
+++ content/back/gastos/derramaAPago.js
@@ -1,6 +1,5 @@
 var derramaAPago = class {
     constructor (mod, obj) {
-        debugger;
         console.log('derramaAPago.js -> constructor');
         this.modulo = mod;
         this.object = obj;
@@ -11,7 +10,7 @@ var derramaAPago = class {
         form.set({pis_comunidad:obj.comunidad});
         form.executeForm();
         form = mod.Forms.formPromesaPago;
-        form.set({nombre:obj.nombre, total:formatoEsp(obj.total, 2)});
+        form.set({detalle_derrama:obj.nombre, total:(obj.total*1), total_derrama:formatoEsp(obj.total, 2)+'€'});
     }
 
     addEventos(mod) {
@@ -22,8 +21,9 @@ var derramaAPago = class {
     }
 
     pintaListaPisos() {
-        let fila = '<tr><td><input type="checkbox" name="vecino" value="{{pis_piso}} data-id="{{pis_piso}}"/></td><td>{{pis_nombre}}</td><td>{{porcentaje}}%</td><td>{{aplicado}}%</td><td>{{imp_total}}</td><td>{{imp_cuota}}</td></tr>';
+        let fila = '<tr><td><input type="checkbox" name="vecino" value="{{pis_piso}}" {{checked}}/></td><td>{{pis_nombre}}</td><td>{{porcentaje}}%</td><td>{{aplicado}}%</td><td>{{imp_total}}</td><td>{{imp_cuota}}</td></tr>';
         let tabla = $('tbody.lista-vecinos');
+        tabla.empty();
         this.listaPisos.forEach(obj => {
             tabla.append(fila.reemplazaMostachos(obj));
         });
@@ -31,38 +31,47 @@ var derramaAPago = class {
 
     //
     recalcular() {
-        let form = this.modulo.Forms.formPromesaPago;
+        let form = this.modulo.Forms.formPromesaPago.parametros;
 
         let cuotas = parseInt(form.num_cuotas.value) || 1;
         let tipo = form.tipo_reparto.value;
-        let importeTotal = this.object.importeTotal;
-
+        let importeTotal = form.total.value;
         // Obtener IDs seleccionados
         let seleccionados = [];
         $('input[name="vecino"]:checked').each((i, chk) => {
-            seleccionados.push($(chk).data('id'));
+            seleccionados.push(chk.value*1);
         });
 
         let numSel = seleccionados.length;
 
+            let sumaCoeficientes = this.listaPisos
+                .filter(p => seleccionados.includes(p.pis_piso))
+                .reduce((acc, p) => acc + parseFloat(p.pis_porcentaje), 0);
         // Recalcular cada piso
         this.listaPisos.forEach(piso => {
-            if (!seleccionados.includes(piso.pis_id)) {
+            if (!seleccionados.includes(piso.pis_piso)) {
+                piso.checked   = '';
                 piso.imp_total = 0;
                 piso.imp_cuota = 0;
+                piso.aplicado = 0;
                 return;
+            } else {
+                piso.checked = 'checked'
+                piso.aplicado = (tipo === 'igual')
+                                    ?(100 / numSel)
+                                    :(piso.pis_porcentaje / sumaCoeficientes) * 100;
             }
-
+            piso.aplicado = Math.round(piso.aplicado * 100) / 100;
             let total = 0;
 
             if (tipo === 'igual') {
                 total = importeTotal / numSel;
             } else {
-                total = importeTotal * (piso.pis_porcentaje / 100);
+                total = importeTotal * (piso.pis_porcentaje / sumaCoeficientes);
             }
 
             let cuota = total / cuotas;
-
+            
             piso.imp_total = formatoEsp(total, 2);
             piso.imp_cuota = formatoEsp(cuota, 2);
         });
@@ -81,6 +90,7 @@ var derramaAPago = class {
             for (let i=0; i<listaPisos.length; i++) {
                 listaPisos[i].imp_total = 0;
                 listaPisos[i].imp_cuota = 0;
+                listaPisos[i].checked   = '';
                 listaPisos[i].porcentaje = formatoEsp(listaPisos[i].pis_porcentaje, 1);
                 listaPisos[i].aplicado = 0;
             }
